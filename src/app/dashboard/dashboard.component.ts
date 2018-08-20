5cdcb0653d95ed90de06ba5cbb64ff53543af59e
Pragati <pragsjainprags@gmail.com>
2018-08-20 19:29:55 +0530

    lazy loading added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,6 +5,9 @@ import { Car } from '../car';
 import { UserService } from '../user.service';
 import { CarsService } from '../cars.service';
 
+ //for lazyloading 
+import {LazyLoadEvent} from 'primeng/primeng';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -19,6 +22,11 @@ export class DashboardComponent implements OnInit {
   baseUrl2: string ='api';
   url2: string ='usersModel';
   UsersModel: string = 'Users Model';
+
+  //for lazyloading 
+  datasource: Car[];
+  loading: boolean;
+  totalRecords: number;
 
   constructor(private userService: UserService, private carsService: CarsService) { }
 
@@ -40,7 +48,32 @@ export class DashboardComponent implements OnInit {
 
   getCars(): void {
     this.carsService.getCars()
-    .subscribe(cars => this.cars = cars);
+    .subscribe(cars => {
+      this.cars = cars;
+
+      //for lazyloading
+      this.datasource = cars;
+      this.totalRecords = this.datasource.length;
+    } );
   }
+
+      loadCarsLazy(event: LazyLoadEvent) {
+        this.loading = true;
+
+        //in a real application, make a remote request to load data using state metadata from event
+        //event.first = First row offset
+        //event.rows = Number of rows per page
+        //event.sortField = Field name to sort with
+        //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+        //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+
+        //imitate db connection over a network
+        setTimeout(() => {
+            if (this.datasource) {
+                this.cars = this.datasource.slice(event.first, (event.first + event.rows));
+                this.loading = false;
+            }
+        }, 1000);
+    }
 
 }
